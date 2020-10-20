1750242e33c1dbbc27b41080dbe65977efb5fa15
Jannis Achstetter <kripton@kripserver.net>
2020-10-20 08:49:10 +0200

    Implement mode switching (text only, clock and countdown) and prepare the actual countdown

--- index.js
+++ index.js
@@ -1,49 +1,78 @@
+// Mumble client
 const NoodleJS = require('noodle.js');
+
+// HTTP server with WebSocket support
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// Save the current mode and message so we can update
+// the clients when they connect and don't have to wait
+// for a new message/command in Mumble
+var state = {
+  text: '[Connecting to Mumble ...]',
+  mode: 'clock',
+  countdownRunning: false,
+  countdownEnd: ''
+}
+
+// Simply serve index.html when asked for /
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+  console.log('NEW  connection from "' + socket.conn.remoteAddress + '" (' +
+    socket.conn.request.headers['user-agent'] + ')');
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  // Send the current state to all connected clients
+  io.emit('update', state);
+
+  // Simply inform when a user disconnects
+  socket.on('disconnect', (reason) => {
+    console.log('LOST connection from "' + socket.conn.remoteAddress + '" (' +
+      reason + ')');
   });
 });
 
 http.listen(8123, () => {
-  console.log('listening on *:8123');
+  console.log('HTTP listening on *:8123');
 });
 
 const client = new NoodleJS({
-    url: '127.0.0.1'
+  url: '127.0.0.1'
 });
 
 client.on('ready', info => {
-    console.log('CONNECTED:', info);
-});
-
-client.voiceConnection.on('end', param => {
-    console.log('end');
+  console.log('Connected to Mumble server:', info);
+  state.text = '[Connected to Mumble]';
 });
 
 client.on('message', message => {
-    console.log(message);
-    if (message.content === 'ping') {
-        message.reply('pong');
-    } else if (message.content === 'C') {
-        client.voiceConnection.playFile('call.mp3');
-        client.voiceConnection.playFile('call.mp3');
-        client.voiceConnection.playFile('call.mp3');
+  console.log(message);
+  if (message.content.startsWith('!')) {
+    if (message.content.startsWith('!text')) {
+      state.mode = 'text';
+    } else if (message.content.startsWith('!clock')) {
+      state.mode = 'clock';
+    } else if (message.content.startsWith('!countdown')) {
+      state.mode = 'countdown';
+      // If no further parameters are given, make it 60 minutes and PAUSE
+      if (message.content === '!countdown') {
+        var now = new Date();
+        var end =  new Date(now.getTime() + 60000);
+        state.countdownEnd = end.toISOString();
+        state.countdownRunning = true;
+      }
+    } else {
+      // Invalid mode
+      client.sendMessage('<span style="color: red;">Invalid command. Valid: <b>!text</b>, <b>!clock<b/>, <b>!countdown</b></span>');
     }
-    io.emit('text', {'text': message.content})
+  } else {
+    state.text = message.content;
+  }
+
+  io.emit('update', state);
 });
 
 client.connect();
